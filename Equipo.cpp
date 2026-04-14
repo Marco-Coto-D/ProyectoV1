5fad1d687fdb1ad055eb96b76b79227c320acecd
Marco <mcotod43@gmail.com>
2026-04-13 21:48:03 -0600

    MD con logica, Base de la clase Equipos **Falta de implementacion de dicha clase

---
+++ Equipo.cpp
@@ -0,0 +1,5 @@
+//
+// Created by marco on 4/13/2026.
+//
+
+#include "Equipo.h"
---
+++ Equipo.h
@@ -0,0 +1,28 @@
+//
+// Created by marco on 4/13/2026.
+//
+
+#ifndef PROYECTOV1_EQUIPO_H
+#define PROYECTOV1_EQUIPO_H
+#include <iostream>
+using namespace std;
+#include <vector>
+
+
+class Equipo {
+private:
+    string nombre;
+    string ID;
+    double estado;
+    int criticidad;
+    int tiempoInactivo;
+    double prioridad; //De este atributo depende cual equipo recibe mantenimiento
+    vector<*Incidencias> incidencias;
+public:
+    void degradar(); //Metodo para degradar el equipo por dia
+    double calcularPrioridad();
+    void aplicarMantenimiento();
+};
+
+
+#endif //PROYECTOV1_EQUIPO_H
